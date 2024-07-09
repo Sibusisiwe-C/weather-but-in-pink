fc92b2c1f720eda18e4ba515d349aefdcfc38fa8
Sibusisiwe-C <sibusisiwenompumelelo202@gmail.com>
2024-07-09 23:23:11 +0200

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "96c5cd8bf41o7951b330f21t2a904051";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Durban");s
